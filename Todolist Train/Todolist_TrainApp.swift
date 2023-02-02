aa9933267194f817b69b64b75d6417515fcb2d28
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-02-02 15:51:19 +0700

    Initial Commit

---
+++ Todolist Train/Todolist_TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todolist_TrainApp.swift
+//  Todolist Train
+//
+//  Created by Putut Yusri Bahtiar on 02/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Todolist_TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
